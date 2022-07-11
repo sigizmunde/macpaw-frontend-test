598efa9c404b1286578650980690bc84c8a6e516
illya bondar <1816579@gmail.com>
2022-07-11 21:50:58 +0300

    feat: axios x-api-key parameter added

--- src/api-service/api.js
+++ src/api-service/api.js
@@ -4,8 +4,9 @@ const API_KEY = 'f987148c-ecf5-480d-acd2-8f3e6d1b392b'; //for file uploading
 const API_BASE_URL = 'https://api.thedogapi.com/v1/images';
 
 axios.defaults.baseURL = API_BASE_URL;
+axios.defaults.headers['x-api-key'] = API_KEY;
 
-export async function fetchDogImages(page) {
+export async function fetchDogImages() {
   const rawData = await axios.get(`/search?size=full`);
   return rawData.data;
 }
