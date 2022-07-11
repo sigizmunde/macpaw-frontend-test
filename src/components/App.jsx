0b75497a9ffee124c1e58de1a1f0ff98b6771f16
illya bondar <1816579@gmail.com>
2022-07-12 00:09:11 +0300

    fix: classes turned into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,25 @@
 import { fetchDogImages } from 'api-service/api';
-import React from 'react';
+import React, { useState } from 'react';
 import Gallery from './Gallery/Gallery';
 
-export class App extends React.Component {
-  state = {
-    items: [],
+export const App = () => {
+  const [items, setItems] = useState([]);
+
+  const loadData = () => {
+    fetchDogImages().then(data => setItems(data));
   };
 
-  loadData = () => {
-    fetchDogImages().then(data => this.setState({ items: data }));
-  };
-
-  handleClick = id => {
+  const handleClick = id => {
     this.setState({ activeId: id });
   };
 
-  render() {
-    const { items } = this.state;
-    return (
-      <div>
-        React test task
-        <button type="button" onClick={this.loadData}>
-          Load
-        </button>
-        {items.length > 0 && (
-          <Gallery items={items} handleClick={this.handleClick} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      React test task
+      <button type="button" onClick={loadData}>
+        Load
+      </button>
+      {items.length > 0 && <Gallery items={items} handleClick={handleClick} />}
+    </div>
+  );
+};
