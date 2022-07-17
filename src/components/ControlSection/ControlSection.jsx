06e2fb73e4c867e5e1368e9bdbe4efdd82f28675
illya bondar <1816579@gmail.com>
2022-07-17 17:24:47 +0300

    feat: dark theme added

--- src/components/ControlSection/ControlSection.jsx
+++ src/components/ControlSection/ControlSection.jsx
@@ -1,14 +1,26 @@
+import Logo from 'components/Logo/Logo';
 import MainMenu from 'components/MainMenu/MainMenu';
-import { Section, H1, Description, Motto } from './ControlSection.styled';
+import {
+  ControlWrapper,
+  Section,
+  H1,
+  Description,
+  Motto,
+  DarkLightButton,
+} from './ControlSection.styled';
 
-const ControlSection = ({ onClick }) => {
+const ControlSection = ({ onClick, handleDarkTheme }) => {
   return (
-    <Section>
-      <H1>Hi intern!</H1>
-      <Description>Welcome to MI 2022 Front-end test</Description>
-      <Motto>Lets start using The Dog API</Motto>
-      <MainMenu onClick={onClick} />
-    </Section>
+    <ControlWrapper>
+      <DarkLightButton onClick={handleDarkTheme} />
+      <Logo onClick={onClick} />
+      <Section>
+        <H1>Hi intern!</H1>
+        <Description>Welcome to MI 2022 Front-end test</Description>
+        <Motto>Lets start using The Dog API</Motto>
+        <MainMenu onClick={onClick} />
+      </Section>
+    </ControlWrapper>
   );
 };
 
--- src/components/ControlSection/ControlSection.styled.js
+++ src/components/ControlSection/ControlSection.styled.js
@@ -1,4 +1,12 @@
+import { SectionWrapper } from 'components/SectionWrapper/SectionWrapper';
 import styled from 'styled-components';
+
+export const ControlWrapper = styled(SectionWrapper)`
+  position: relative;
+  @media screen and (max-width: 767px) {
+    position: absolute;
+  }
+`;
 
 export const Section = styled.section`
   width: 446px;
@@ -7,6 +15,7 @@ export const Section = styled.section`
 `;
 
 export const H1 = styled.h1`
+  margin-top: 0;
   margin-bottom: 10px;
   font-style: normal;
   font-weight: 500;
@@ -29,4 +38,41 @@ export const Motto = styled.p`
   font-weight: 500;
   font-size: 20px;
   line-height: 1.45;
+`;
+
+export const DarkLightButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10%;
+  display: flex;
+  justify-content: stretch;
+  align-items: stretch;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  padding: auto 10px;
+  min-width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 10px;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  color: var(--inverted-text-color);
+  background-color: var(--accent-color);
+  :hover {
+    color: var(--accent-color);
+    background-color: var(--faded-accent-color);
+  }
+  :active {
+    color: var(--accent-color);
+    background-color: var(--panel-back-color);
+  }
+  ::before {
+    content: var(--theme-icon);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
