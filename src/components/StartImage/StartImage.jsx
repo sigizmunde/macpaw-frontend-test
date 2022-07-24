41a75f926097191dc4a194ce847b8ba79d89a5a6
illya bondar <1816579@gmail.com>
2022-07-24 12:19:56 +0300

    fix: start page markup fixed

--- src/components/StartImage/StartImage.jsx
+++ src/components/StartImage/StartImage.jsx
@@ -1,10 +1,13 @@
+import { SectionWrapper } from 'components/SectionWrapper/SectionWrapper';
 import { StartPanel, StartImg } from './StartImage.styled';
 
 const StartImage = () => {
   return (
-    <StartPanel>
-      <StartImg src={require('../../images/start.png')} alt="head image" />
-    </StartPanel>
+    <SectionWrapper>
+      <StartPanel>
+        <StartImg src={require('../../images/start.png')} alt="head image" />
+      </StartPanel>
+    </SectionWrapper>
   );
 };
 
